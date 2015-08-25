48e68f86e780ff9cfee70012bbc53b2377114e60
robsnakes <andrewjohnrobinson@gmail.com>
2015-08-25 11:46:46 +0100

    Update ApiClientConfiguration.java
    Support for proxy

--- src/main/java/com/okta/sdk/framework/ApiClientConfiguration.java
+++ src/main/java/com/okta/sdk/framework/ApiClientConfiguration.java
@@ -3,12 +3,23 @@ package com.okta.sdk.framework;
 public class ApiClientConfiguration {
 
     private String baseUrl;
+    private String proxyUrl;
+    private int proxyPort;
+    private String proxyScheme;
     private int apiVersion = 1;
     private String apiToken;
 
     public ApiClientConfiguration(String baseUrl, String apiToken) {
         this.baseUrl = baseUrl;
         this.apiToken = apiToken;
+    }
+
+    public ApiClientConfiguration(String baseUrl, String apiToken, String proxyUrl, int proxyPort, String proxyScheme) {
+        this.baseUrl = baseUrl;
+        this.apiToken = apiToken;
+        this.proxyUrl = proxyUrl;
+        this.proxyPort = proxyPort;
+        this.proxyScheme = proxyScheme;
     }
 
     public String getBaseUrl() {
@@ -29,5 +40,17 @@ public class ApiClientConfiguration {
 
     public void setApiToken(String apiToken) {
         this.apiToken = apiToken;
+    }
+
+    public String getProxyUrl() {
+        return proxyUrl;
+    }
+
+    public int getProxyPort() {
+        return proxyPort;
+    }
+
+    public String getProxyScheme() {
+        return proxyScheme;
     }
 }
